728266381d81696536d430a31219ee3c8cb19217
z1nkx <z1nkxmail@gmail.com>
2023-02-08 21:40:30 +1000

    border

--- components/GitHubLink.tsx
+++ components/GitHubLink.tsx
@@ -6,15 +6,7 @@ export default function ThirdwebGuideFooter() {
     <>
       <div
         style={{
-          position: "fixed",
-          bottom: -120,
-          right: -80,
-          height: 300,
-          width: 150,
-          border: "0px solid #eaeaea",
-          transform: "rotate(45deg)",
-          backgroundColor: " #000000",
-          cursor: "",
+        
         }}
         role=""
       />
